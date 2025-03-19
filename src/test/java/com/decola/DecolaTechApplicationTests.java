6312d46e667b1675c67fc9b0c0c9a316bc508152
BBRRUUNNOO <brunoricardo050@gmail.com>
2025-03-19 14:17:38 -0300

    projeto java restful API

---
+++ src/test/java/com/decola/DecolaTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecolaTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
